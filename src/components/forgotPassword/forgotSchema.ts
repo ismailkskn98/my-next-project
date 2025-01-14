3943f0e4fbdec3daad6205aa48e3efde91a3583a
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-01-14 12:10:08 +0300

    gereksiz dosyalar silindi

--- src/components/forgotPassword/forgotSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-const forgotSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Geçerli bir e-posta adresi giriniz")
-    .required("E-posta gereklidir"),
-});
-
-export default forgotSchema;
